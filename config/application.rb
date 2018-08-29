885cec831f91e53b6863dedc24eb85230f37bfc5
Sakshi S <heysakshi@gmail.com>
2018-08-29 22:44:54 +0530

    changes for week 5

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,18 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require_relative '../lib/timer'
+
+require 'record_tag_helper' 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Blog
-  class Application < Rails::Application
+  class Application < Rails::Application 
+    config.middleware.insert_before(Rack::Sendfile, Timer)
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
